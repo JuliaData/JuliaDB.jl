7ecf06270534320db7232afad8a58965782fa003
Shashi Gowda <shashi@juliacomputing.com>
2017-02-26 11:15:14 +0530

    scalar getindex, some indexing fixes, more tests

--- src/dtable.jl
+++ src/dtable.jl
@@ -28,6 +28,7 @@ metadata about an NDSparse chunk
 """
 immutable TableDomain{T<:IndexTuple} <: Domain
     interval::Interval{T}
+    boundingrect::Interval{T}
     nrows::Nullable{Int}
 end
 
@@ -40,7 +41,11 @@ end
 
 function Dagger.domain(nd::NDSparse)
     if !isempty(nd)
-        TableDomain(first(nd.index), last(nd.index), Nullable{Int}(length(nd)))
+        interval = Interval(first(nd.index), last(nd.index))
+        cs = astuple(nd.index.columns)
+        extr = map(extrema, cs)
+        boundingrect = Interval(map(first, extr), map(last, extr))
+        TableDomain(interval, boundingrect, Nullable{Int}(length(nd)))
     else
         EmptyDomain{eltype(nd.index)}()
     end
@@ -49,23 +54,34 @@ end
 # many methods of NDSparse that only need the info in a TableDomain
 Base.eltype{T}(::TableDomain{T}) = T
 Base.eltype{T}(::EmptyDomain{T}) = T
+
 Base.isempty(::EmptyDomain) = true
 Base.isempty(::TableDomain) = false
+
 nrows(td::TableDomain) = td.nrows
 nrows(td::EmptyDomain) = Nullable(0)
+
 Base.length(td::TableDomain) = get(td.nrows) # well when it works
 Base.length(td::EmptyDomain) = 0
+
 Base.ndims{T}(::TableDomain{T})  = nfields(T)
 Base.ndims{T}(::EmptyDomain{T})  = nfields(T)
+
 Base.first(td::TableDomain) = first(td.interval)
 Base.last(td::TableDomain) = last(td.interval)
+
+mins(td::TableDomain) = first(td.boundingrect)
+maxes(td::TableDomain) = last(td.boundingrect)
+
 function Base.merge(d1::TableDomain, d2::TableDomain, collisions=false)
     n = collisions || isnull(d1.nrows) || isnull(d2.nrows) ?
         Nullable{Int}() :
         Nullable(get(d1.nrows) + get(d2.nrows))
-    TableDomain(min(first(d1), first(d2)), max(last(d1), last(d2)), n)
-end
 
+    interval = Interval(min(first(d1), first(d2)), max(last(d1), last(d2)))
+    boundingrect = Interval(map(min, mins(d1), mins(d2)), map(max, maxes(d1), maxes(d2)))
+    TableDomain(interval, boundingrect, n)
+end
 Base.merge(d1::TableDomain, d2::EmptyDomain) = d1
 Base.merge(d1::EmptyDomain, d2::Union{TableDomain, EmptyDomain}) = d2
 
@@ -74,12 +90,15 @@ Base.merge(d1::EmptyDomain, d2::Union{TableDomain, EmptyDomain}) = d2
 Create an `NDSparse` lookup table from a bunch of `TableDomain`s
 """
 function chunks_index(subdomains, chunks, lengths)
+
     index = Columns(map(x->Array{Interval{typeof(x)}}(0),
                         first(subdomains[1].interval))...)
+
     for subd in subdomains
         int=subd.interval
         push!(index, map(Interval, first(int), last(int)))
     end
+
     NDSparse(index, Columns(chunks, lengths, names=[:chunk, :length]))
 end
 
@@ -90,16 +109,18 @@ Convenience function to create a DTable from an array of chunks.
 The chunks must be non-Thunks. Omits empty chunks in the output.
 """
 function fromchunks(chunks::AbstractArray)
+
     subdomains = map(domain, chunks)
     nzidxs = find(x->!isempty(x), subdomains)
     subdomains = subdomains[nzidxs]
     chunks = chunks[nzidxs]
+
     DTable(Cat(promote_type(map(chunktype, chunks)...),
-        reduce(merge, subdomains),
-        nothing,
-        chunks_index(subdomains, chunks, map(nrows, subdomains)),
-       )
-     )
+            reduce(merge, subdomains),
+            nothing,
+            chunks_index(subdomains, chunks, map(nrows, subdomains)),
+        )
+    )
 end
 
 ### Distribute a NDSparse into a DTable
@@ -146,9 +167,8 @@ end
 
 # util
 function subdomain(nds, r)
-    TableDomain(nds.index[first(r)],
-                nds.index[last(r)],
-                Nullable(length(nds.index[r])))
+    # TODO: speed it up
+    domain(subtable(nds, r))
 end
 
 function withchunksindex(f, dt::DTable)
