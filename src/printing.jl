6b44b2872642b911578a12a1e1ee47fde863a60d
Shashi Gowda <shashi@juliacomputing.com>
2017-03-04 20:05:14 +0530

    take out some data and ask IndexedTables to print. closes #7

---
+++ src/printing.jl
@@ -0,0 +1,17 @@
+const TextMIME = Union{MIME"text/plain", MIME"text/html"}
+function take_n(t::DTable, n, dir=1)
+    i = dir == -1 ? n : 1
+    chunkcol = chunks(t).data.columns.chunk
+    top = gather(chunkcol[i])
+    while length(top) < n && 1 <= i <= length(chunkcol)
+        i += dir
+        top = merge(top, gather(chunkcol[i]))
+    end
+    return top
+end
+
+function Base.show(io::IO, t::DTable)
+    # we fetch at most 21 elements and let NDSparse
+    # display it.
+    show(io, merge(take_n(t, 11), take_n(t, 10, -1)))
+end
