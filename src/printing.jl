96406908bb98ae75d7cc6476db4b5c31f6e7e157
Shashi Gowda <shashi@juliacomputing.com>
2017-03-06 17:06:09 +0530

    printing fixes

--- src/printing.jl
+++ src/printing.jl
@@ -1,6 +1,6 @@
 const TextMIME = Union{MIME"text/plain", MIME"text/html"}
 function take_n(t::DTable, n, dir=1)
-    i = dir == -1 ? n : 1
+    i = dir == -1 ? length(t.chunks) : 1
     chunkcol = chunks(t).data.columns.chunk
     top = gather(chunkcol[i])
     while length(top) < n && 1 <= i <= length(chunkcol)
@@ -13,5 +13,9 @@ end
 function Base.show(io::IO, t::DTable)
     # we fetch at most 21 elements and let NDSparse
     # display it.
-    show(io, merge(take_n(t, 11), take_n(t, 10, -1)))
+    if !isempty(t.chunks)
+        show(io, merge(take_n(t, 11), take_n(t, 10, -1)))
+    else
+        println(io, "an empty table")
+    end
 end
