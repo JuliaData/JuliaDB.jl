c3095dcb41522609079fcc5e80a426cc729811c5
Shashi Gowda <shashi@juliacomputing.com>
2017-03-09 14:48:44 +0530

    ingest: parse CSV data and save it to mmappable binary files. load and save for any DTable

--- src/JuliaDB.jl
+++ src/JuliaDB.jl
@@ -7,8 +7,10 @@ export NDSparse, Columns
 
 include("util.jl")
 include("interval.jl")
+include("mmap.jl")
 include("dtable.jl")
 include("load.jl")
+include("load-save.jl")
 include("printing.jl")
 
 include("indexing.jl")
