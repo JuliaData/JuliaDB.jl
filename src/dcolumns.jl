f91bfdeb43959b401f8fa6c8a1b28e67c885c5ad
Shashi Gowda <shashi@juliacomputing.com>
2017-03-10 02:51:51 +0530

    extract columns as Dagger.ComputedArray

---
+++ src/dcolumns.jl
@@ -0,0 +1,25 @@
+# Extract a column as a Dagger array
+export getindexcol, getdatacol
+
+import IndexedTables: DimName
+import Dagger: DomainBlocks, ArrayDomain, Cat, ComputedArray
+
+function extractarray(t::DTable, accessor)
+    arraymaker = function (cs_tup...)
+        cs = [cs_tup...]
+        lengths = length.(domain.(cs))
+        dmnchunks = DomainBlocks((1,), (cumsum(lengths),))
+        Cat(chunktype(cs[1]), ArrayDomain(1:sum(lengths)), dmnchunks, [cs...])
+    end
+
+    cs = map(delayed(accessor), chunks(t).data.columns.chunk)
+    ComputedArray(compute(delayed(arraymaker;meta=true)(cs...)))
+end
+
+function getindexcol(t::DTable, dim::DimName)
+    extractarray(t, nd -> nd.index.columns[dim])
+end
+
+function getdatacol(t::DTable, dim::DimName)
+    extractarray(t, nd -> nd.data.columns[dim])
+end
