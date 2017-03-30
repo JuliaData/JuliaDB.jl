d1c0a3bb0d085fddabcffa2836747b0a0251ec95
Jeff Bezanson <jeff.bezanson@gmail.com>
2017-03-30 17:28:47 -0400

    automatically add a 1:n index space if there are no indices

--- src/load.jl
+++ src/load.jl
@@ -79,9 +79,26 @@ function loadfiles(files::AbstractVector, delim=','; usecache=true, opts...)
     data = map(delayed(load_f), unknown)
 
     chunkrefs = gather(delayed(vcat)(data...))
+
+    if !isnull(chunkrefs[1].handle.offset)
+        # implicit index mode - fabricate a 1-d index space 1:n
+        if metadata === nothing
+            o = 1
+        else
+            o = last(metadata.data.columns.metadata[end].domain)[1] + 1
+        end
+        for c in chunkrefs
+            c.handle.offset = o
+            n = get(nrows(domain(c)))
+            c.domain = IndexSpace(Interval((o,), (o+n-1,)),
+                                  Interval((o,), (o+n-1,)), Nullable{Int}(n))
+            o += n
+        end
+    end
+
     # store this back in cache
     cache = Table(Columns(unknown, fill(opthash, length(unknown)), names=[:filename, :opthash]),
-                     Columns(mtime.(unknown), Dagger.Chunk[chunkrefs...], names=[:mtime, :metadata]))
+                  Columns(mtime.(unknown), Dagger.Chunk[chunkrefs...], names=[:mtime, :metadata]))
 
     if metadata != nothing
         cache = merge(metadata, cache)
@@ -102,6 +119,7 @@ type CSVChunk
     delim::Char
     cached_on::Vector{Int}
     opts::Dict
+    offset::Nullable{Int}  # index of first item when using implicit indices
 end
 
 Dagger.affinity(c::CSVChunk) = map(OSProc, c.cached_on)
@@ -109,26 +127,38 @@ Dagger.affinity(c::CSVChunk) = map(OSProc, c.cached_on)
 function gather(ctx, csv::CSVChunk)
     if csv.cache && haskey(_read_cache, csv.filename)
         #println("Having to fetch data from $csv.cached_on")
-        _read_cache[csv.filename]
+        data = _read_cache[csv.filename]
     elseif csv.cached_on != [myid()] && !isempty(csv.cached_on)
         # TODO: remove myid() if it's in cached_on
         pid = first(csv.cached_on)
         if pid == myid()
             pid = last(csv.cached_on)
         end
-        remotecall_fetch(c -> gather(ctx, c), pid, csv)
+        data = remotecall_fetch(c -> gather(ctx, c), pid, csv)
     else
         #println("CACHE MISS $csv")
-        data = loadTable(csv.filename, csv.delim; csv.opts...)
+        data, ii = loadTable(csv.filename, csv.delim; csv.opts...)
+
+        if ii && isnull(csv.offset)
+            csv.offset = 1
+        end
+
         if !(myid() in csv.cached_on)
             push!(csv.cached_on, myid())
         end
         _read_cache[csv.filename] = data
     end
+
+    if !isnull(csv.offset) && data.index[1][1] != get(csv.offset)
+        o = get(csv.offset)
+        data.index.columns[1][:] = o:(o+length(data)-1)
+    end
+
+    return data
 end
 
 function makecsvchunk(file, delim; cache=true, opts...)
-    handle = CSVChunk(file, cache, delim, Int[], Dict(opts))
+    handle = CSVChunk(file, cache, delim, Int[], Dict(opts), nothing)
     # We need to actually load the data to get things like
     # the type and Domain. It will get cached if cache is true
     nds = gather(Context(), handle)
