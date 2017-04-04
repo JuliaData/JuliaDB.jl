426a5c3d81f7519f4c27df76cbafa3295659a5e5
Shashi Gowda <shashi@juliacomputing.com>
2017-04-04 19:37:54 +0530

    fix failure due to sort bringing chunks into memory

--- src/load-save.jl
+++ src/load-save.jl
@@ -82,7 +82,16 @@ function ingest!(files::Union{AbstractVector,String}, outputdir::AbstractString;
     saved = map(delayed(load_and_save), files)
 
     chunks = vcat(prev_chunks, gather(delayed(vcat)(saved...)))
+    filenames1 = map(c -> c.handle.filename, chunks)
     dtable = fromchunks(chunks)
+
+    if any(c->!isa(c.handle, OnDisk), dtable.chunks.data.columns.chunk)
+        # This means `fromchunks` had to re-sort overlapping chunks
+        dtable = save(dtable, outputdir) # write out the sorted version
+        filenames2 = map(c -> c.handle.filename, dtable.chunks.data.columns.chunk)
+        Base.foreach(rm, setdiff(filenames1, filenames2))
+    end
+
     open(io -> serialize(io, dtable), joinpath(outputdir, JULIADB_INDEXFILE), "w")
     dtable
 end
