e5d0d9083b9f0faca172e7385f684581f2521d83
Shashi Gowda <shashi@juliacomputing.com>
2017-04-05 15:43:42 +0530

    get subspaces of chunks after sort. make sort out-of-core

--- src/sort.jl
+++ src/sort.jl
@@ -1,6 +1,6 @@
 import Base.Sort: Forward, Ordering, Algorithm, defalg, lt
 
-function _sort(t::DTable;
+function _sort{K,V}(t::DTable{K,V};
                alg::Algorithm=defalg([1]),
                lt=Base.isless,
                by=identity,
@@ -11,16 +11,15 @@ function _sort(t::DTable;
     computed_t = compute(ctx, t, true) # This might have overlapping chunks
 
     lengths = map(get, chunks(computed_t).data.columns.length)
-    splitter_ranks = cumsum(lengths)[1:end-1]  # Get ranks for splitting at
+    ranks = cumsum(lengths)[1:end-1]  # Get ranks for splitting at
 
     idx = dindex(computed_t).result
-    # note: here we assume that each chunk is already sorted.
-    splitters = Dagger.select(ctx, idx, splitter_ranks, order)
+    # select elements of required ranks in parallel:
+    splitters = Dagger.pselect(ctx, idx, ranks, order)
 
     cs = chunks(computed_t).data.columns.chunk
-    thunks = shuffle_merge(ctx, cs, splitters, lengths, order)
-    #delayed(println)(thunks...)
-    compute(ctx, delayed((x...)->fromchunks([x...]); meta=true)(thunks...))
+    thunks, subspaces = shuffle_merge(ctx, cs, ranks, splitters, lengths, order)
+    fromchunks(thunks, subspaces; KV=(K,V))
 end
 
 Dagger.mid(x::NamedTuple, y::NamedTuple) = map(Dagger.mid, x, y)
@@ -31,26 +30,43 @@ function merge_thunk(cs, starts, lasts, empty, ord)
     if isempty(nonempty)
         empty
     else
-        delayed(_merge)(map(delayed(subtable), cs[nonempty], ranges[nonempty])...)
+        subspaces = gather(delayed(vcat)(map(delayed(subindexspace),
+                                             cs[nonempty], ranges[nonempty])...))
+        thunk = delayed(_merge)(map(delayed(subtable), cs[nonempty], ranges[nonempty])...)
+        thunk, reduce(merge, subspaces)
     end
 end
 
-function shuffle_merge(ctx, cs, splitter_indices, lengths, ord)
+function shuffle_merge(ctx, cs, ranks, splitter_indices, lengths, ord)
     # splitter_indices: array of (splitter => vector of p index ranges) in sorted order
     starts = ones(Int, length(cs))
 
     empty = delayed(x->subtable(x, 1:0))(cs[1]) # An empty table with the right types
 
     merged_chunks = [begin
-        lasts = map(last, idxs)
-        thnk = merge_thunk(cs, starts, lasts, empty, ord)
-        #sz = sum(lasts.-starts.+1)
+        lasts = map(first, idxs).-1 # First, all elements less than that of the required rank
+        i = 1
+        while sum(lasts) < rank
+            reqd = rank - sum(lasts)
+            if i > length(idxs)
+                error("Median of wrong rank found")
+            end
+            available = min(reqd, length(idxs[i]))
+            lasts[i] += available
+            i += 1
+        end
+
+        thnk, subspace = merge_thunk(cs, starts, lasts, empty, ord)
         starts = lasts.+1
-        thnk#,sz
-    end for (val, idxs) in splitter_indices]
+        thnk, subspace
+
+        end for (rank, idxs) in zip(ranks, map(last, splitter_indices))]
 
     # trailing sub-chunks make up the last chunk:
-    thunks = vcat(merged_chunks, merge_thunk(cs, starts, lengths, empty, ord))
+    t, s = merge_thunk(cs, starts, lengths, empty, ord)
+    thunks = vcat(map(first, merged_chunks), t)
+    subspaces = vcat(map(last, merged_chunks), s)
+    thunks, subspaces
 end
 
 
