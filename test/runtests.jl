bd679181208aed5d02a1deee8ad2de7a16dc55b7
Shashi Gowda <shashi@juliacomputing.com>
2017-04-12 20:44:57 +0530

    add a way to load data to memory instead of mmapping

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,6 @@ using IndexedTables
 using NamedTuples
 using PooledArrays
 
-import JuliaDB: MmappableArray, copy_mmap, unwrap_mmap, tuplesetindex
-
 include("test_util.jl")
 include("test_query.jl")
 include("test_join.jl")
