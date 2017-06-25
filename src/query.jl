a86a6c5a51c28f8e7ca74ddf2c27a82679b2c73c
Shashi Gowda <shashi@juliacomputing.com>
2017-06-25 23:50:10 +0530

    export aggregate_vec

--- src/query.jl
+++ src/query.jl
@@ -1,4 +1,4 @@
-export select, convertdim, aggregate, reducedim_vec
+export select, convertdim, aggregate, reducedim_vec, aggregate_vec
 
 import IndexedTables: convertdim, aggregate, aggregate_vec, reducedim_vec, pick
 import Base: reducedim
