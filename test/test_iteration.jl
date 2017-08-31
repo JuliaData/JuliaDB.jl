8cd2b79c5f723ac7cf1e87452e52f6e47cd6d11e
Shashi Gowda <shashi@juliacomputing.com>
2017-08-31 18:22:02 +0530

    fix hanging test

--- test/test_iteration.jl
+++ test/test_iteration.jl
@@ -1,3 +1,6 @@
+using Base.Test
+using JuliaDB
+
 @testset "iteration" begin
     x = distribute(IndexedTable(Columns(a=[1,1], b=[1,2]), Columns(c=[3,4])), 2)
     y = distribute(IndexedTable(Columns(a=[1,1], b=[1,2]), [3,4]), 2)
@@ -8,7 +11,6 @@
     @test column(x, 3) == [3,4]
     @test column(y, 3) == [3,4]
 
-    @test columns(x, :a) == [1,1]
     @test columns(x) == @NT(a=[1,1], b=[1,2], c=[3,4])
     @test columns(x, (:a,:c)) == @NT(a=[1,1], c=[3,4])
     @test columns(y, (1, 3)) == ([1,1], [3,4])
@@ -27,7 +29,7 @@
     @test values(x,(1,1)) == [(3,3), (4,4)]
     @test values(y) == [3, 4]
     @test values(y,1) == [3,4]
-    @test values(y,as(reverse, 1, :x)) == [4, 3]
+    @test values(y,as(x->compute(sort(x, rev=true)), 1, :x)) == [4, 3]
     @test values(y,(as(-, 1, :x),)) == [@NT(x=-3), @NT(x=-4)]
 
     @test collect(pairs(x)) == [@NT(a=1,b=1)=>@NT(c=3), @NT(a=1,b=2)=>@NT(c=4)]
