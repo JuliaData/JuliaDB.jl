f560c45aff6bcdd909589376da70eac43f51b234
Shashi Gowda <shashi@juliacomputing.com>
2017-10-30 19:36:15 +0530

    use tuple

--- src/query.jl
+++ src/query.jl
@@ -187,7 +187,7 @@ function mapslices(f, x::DTable, dims; name=nothing)
     end
 
     t = has_overlaps(x.subdomains, iterdims) ?
-        rechunk(x, closed=true, by=iterdims) : x
+        rechunk(x, closed=true, by=(iterdims...)) : x
 
     cache_thunks(mapchunks(y -> mapslices(f, y, dims, name=name),
                            t, keeplengths=false))
