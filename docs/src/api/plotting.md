020d89c20d18182b72e60c9d84159a670a0923ee
Josh Day <emailjoshday@gmail.com>
2018-01-12 16:07:51 -0500

    (almost) start from scratch

--- docs/src/api/plotting.md
+++ docs/src/api/plotting.md
@@ -11,15 +11,8 @@ t = table(@NT(x = randn(100), y = randn(100)))
 @df t scatter(:x, :y)
 ```
 
-For handling huge data, JuliaDB also adds `partitionplot` for plotting summaries of data 
-columns, optionally grouped by another column.
+## `partitionplot`
 
-```
-using JuliaDB, Plots, OnlineStats
-
-t = table(@NT(x = randn(10^6), y = rand(Bool, 10^6)))
-
-partitionplot(t, :x)
-
-partitionplot(t, :x, by = :y, nparts = 200, stat = Extrema())
+```@docs
+partitionplot
 ```
--- src/recipes.jl
+++ src/recipes.jl
@@ -1,36 +1,74 @@
 using RecipesBase
 
+export partitionplot
+
 #-----------------------------------------------------------------# partitionplot
 @userplot struct PartitionPlot
     args
 end
 
-@recipe function f(tp::PartitionPlot; nparts = 50, stat = nothing, by = nothing)
-    t = tp.args[1]
-    selection = tp.args[2]
-    o = (stat == nothing) ? 
-            make_stat(fieldtype(eltype(t), IndexedTables.colindex(t, selection))) : 
-            stat 
-    if by == nothing 
-        @series begin 
-            title --> selection
-            reduce(Partition(o, nparts), t; select = selection)
+@recipe function f(o::PartitionPlot; nparts = 100, stat = nothing, by = nothing)
+    t = o.args[1]
+    sel_x = o.args[2] 
+    stat = (stat == nothing) ? Extrema() : stat
+    if length(o.args) == 3
+        sel_y = o.args[3]
+        T = fieldtype(eltype(t), IndexedTables.colindex(t, sel_x))
+        if by == nothing 
+            label --> OnlineStats.name(stat,false,false) * " of $sel_y"
+            reduce(IndexedPartition(T, stat, nparts), t; select = (sel_x, sel_y))
+        else 
+            out = collect(groupreduce(IndexedPartition(T, stat, nparts), t, by; select = (sel_x, sel_y)))
+            for i in 1:length(out)
+                @series begin 
+                    label --> OnlineStats.name(stat,false,false) * " of $(out[i][1])"
+                    out[i][2]
+                end
+            end
         end
-    else
-        out = collect(groupreduce(Partition(o, nparts), t, by; select = selection))
-        layout := length(out)
-        for i in 1:length(out)
-            @series begin 
-                title --> string(selection) * " ($(out[i][1]))"
-                subplot --> i
-                out[i][2]
+    elseif length(o.args) == 2 
+        if by == nothing
+            label --> OnlineStats.name(stat,false,false) * " of $sel_x"
+            reduce(Partition(stat, nparts), t; select = sel_x)
+        else 
+            out = groupreduce(Partition(stat, nparts), t; select = sel_x)
+            for i in 1:length(out)
+                @series begin 
+                    label --> OnlineStats.name(stat,false,false) * " of $(out[i][1])"
+                    out[i][2]
+                end
             end
         end
     end
 end
 
-# Default OnlineStat based on data type
-make_stat(::Type{<:Number}) = Mean()
-make_stat(::Type{T})  where {T<:Union{AbstractString, Symbol, Bool}} = CountMap(T)
+"""
+    partitionplot(table, y;    stat=Extrema(), nparts=100, by=nothing)
+    partitionplot(table, x, y; stat=Extrema(), nparts=100, by=nothing)
 
-make_stat(T) = error("No predefined stat for $T")
+Plot a summary of variable `y` against `x` (`1:length(y)` if not specified).  Using `nparts`
+approximately equal sections along the x-axis, the data in `y` over the section is 
+summarized by `stat`. 
+
+# Examples 
+
+```
+using JuliaDB, Plots, OnlineStats
+
+x = randn(10^6)
+y = x + randn(10^6)
+z = x .> 0
+t = table(@NT(x=x, y=y, z=z))
+
+# x by itself
+partitionplot(t, :x, stat = Extrema())
+
+# y by x
+partitionplot(t, :x, :y, stat = Hist(25))
+
+# y by x, grouped by z
+partitionplot(t, :x, :y, stat = Extrema(), by = z)
+```
+"""
+partitionplot
+
