2cfee0a02678628696c1a5cee620782e31974b50
Josh Day <joshday@users.noreply.github.com>
2018-02-01 16:21:31 -0500

    Add dropmissing option partitionplot (#126)
    * add dropmissing keyword arg in partitionplot
    
    * better groupby label

--- src/recipes.jl
+++ src/recipes.jl
@@ -7,34 +7,44 @@ export partitionplot
     args
 end
 
-@recipe function f(o::PartitionPlot; nparts = 100, stat = nothing, by = nothing)
+getvalue(x) = x 
+getvalue(x::DataValues.DataValue) = get(x)
+
+indextype(x::Type) = x 
+indextype(x::Type{DataValues.DataValue{T}}) where {T} = T
+
+@recipe function f(o::PartitionPlot; nparts = 100, stat = nothing, by = nothing, dropmissing = false)
     t = o.args[1]
     sel_x = o.args[2] 
     stat = (stat == nothing) ? Extrema() : stat
     if length(o.args) == 3
         sel_y = o.args[3]
-        T = fieldtype(eltype(t), IndexedTables.colindex(t, sel_x))
+        T = indextype.(fieldtype(eltype(t), IndexedTables.colindex(t, sel_x)))
+        s = dropmissing ? 
+            series(IndexedPartition(T, stat, nparts); filter = x->all(!isnull, x), transform = x->getvalue.(x)) :
+            series(IndexedPartition(T, stat, nparts))
         if by == nothing 
-            label --> OnlineStats.name(stat,false,false) * " of $sel_y"
-            reduce(IndexedPartition(T, stat, nparts), t; select = (sel_x, sel_y))
+            reduce(s, t; select = (sel_x, sel_y))
         else 
-            out = collect(groupreduce(IndexedPartition(T, stat, nparts), t, by; select = (sel_x, sel_y)))
+            out = collect(groupreduce(s, t, by; select = (sel_x, sel_y)))
             for i in 1:length(out)
                 @series begin 
-                    label --> OnlineStats.name(stat,false,false) * " of $(out[i][1])"
+                    label --> OnlineStats.name(stat,false,false) * " of $sel_x ($(out[i][1]))"
                     out[i][2]
                 end
             end
         end
     elseif length(o.args) == 2 
+        s = dropmissing ? 
+            series(Partition(stat, nparts); filter = !isnull, transform = getvalue) :
+            series(Partition(stat, nparts))
         if by == nothing
-            label --> OnlineStats.name(stat,false,false) * " of $sel_x"
-            reduce(Partition(stat, nparts), t; select = sel_x)
+            reduce(s, t; select = sel_x)
         else 
-            out = groupreduce(Partition(stat, nparts), t, by; select = sel_x)
+            out = groupreduce(s, t, by; select = sel_x)
             for i in 1:length(out)
                 @series begin 
-                    label --> OnlineStats.name(stat,false,false) * " of $(out[i][1])"
+                    label --> OnlineStats.name(stat,false,false) * " of $sel_x ($(out[i][1]))"
                     out[i][2]
                 end
             end
@@ -43,8 +53,8 @@ end
 end
 
 """
-    partitionplot(table, y;    stat=Extrema(), nparts=100, by=nothing)
-    partitionplot(table, x, y; stat=Extrema(), nparts=100, by=nothing)
+    partitionplot(table, y;    stat=Extrema(), nparts=100, by=nothing, dropmissing=false)
+    partitionplot(table, x, y; stat=Extrema(), nparts=100, by=nothing, dropmissing=false)
 
 Plot a summary of variable `y` against `x` (`1:length(y)` if not specified).  Using `nparts`
 approximately-equal sections along the x-axis, the data in `y` over each section is 
