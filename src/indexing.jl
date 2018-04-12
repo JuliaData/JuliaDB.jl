c5fc90274506e4dc8ca5be281170b9306464b89e
Shashi Gowda <shashi@juliacomputing.com>
2018-04-12 20:01:27 +0530

    make indexing not use bounding rect

--- src/indexing.jl
+++ src/indexing.jl
@@ -20,18 +20,13 @@ end
 
 function _getindex_scalar(t::DNDSparse{K,V}, idxs) where {K,V}
     # scalar getindex
-    brects = boundingrect.(t.domains)
-    function shouldlook(rect)
-        for i in 1:nfields(idxs)
-            if !(idxs[i] in Interval(rect.first[i], rect.last[i]))
-                return false
-            end
-        end
-        return true
+    rng = map(x->[x], idxs)
+    t = collect(fromchunks(delayedmap(x->x[rng...], t.chunks)))
+    if isempty(t)
+        throw(KeyError(idxs))
+    else
+        first(t)
     end
-    subchunk_idxs = find(shouldlook, brects)
-    t1 = DNDSparse{K,V}(t.domains[subchunk_idxs], t.chunks[subchunk_idxs])
-    collect(t1)[idxs...]
 end
 
 function _getindex(t::DNDSparse{K,V}, idxs) where {K,V}
@@ -44,10 +39,6 @@ function _getindex(t::DNDSparse{K,V}, idxs) where {K,V}
 
     # Subset the chunks
     # this is currently a linear search
-
-    brects = boundingrect.(t.domains)
-    subchunk_idxs = find(c->all(map(in, idxs, map(Interval, c.first, c.last))), brects)
-    t = DNDSparse{K,V}(t.domains[subchunk_idxs], t.chunks[subchunk_idxs])
 
     mapchunks(t, keeplengths=false) do chunk
         getindex(chunk, idxs...)
